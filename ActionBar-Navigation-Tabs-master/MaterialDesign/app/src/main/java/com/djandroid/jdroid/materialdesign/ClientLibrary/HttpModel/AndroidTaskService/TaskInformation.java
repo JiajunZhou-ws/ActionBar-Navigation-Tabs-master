4cd074e73650e06aa6d74ec2206df2145b643edb
Jimmy Wang <yongjiewang@live.com>
2016-11-07 23:03:58 +0800

    update taskinformation

--- ActionBar-Navigation-Tabs-master/MaterialDesign/app/src/main/java/com/djandroid/jdroid/materialdesign/ClientLibrary/HttpModel/AndroidTaskService/TaskInformation.java
+++ ActionBar-Navigation-Tabs-master/MaterialDesign/app/src/main/java/com/djandroid/jdroid/materialdesign/ClientLibrary/HttpModel/AndroidTaskService/TaskInformation.java
@@ -7,11 +7,13 @@ import com.djandroid.jdroid.materialdesign.ClientLibrary.Parameter.AuditStatus;
  * Created by Jimmy on 2016/11/1.
  */
 public class TaskInformation {
-    public String idTask; //not visible to user
+    public String projectName;
+    public String idTask;
     public String region;
     public String siteid;
     public String sitename;
     public String siteaddress;
+    public String subcontractor;
     public AuditStatus status;
     public String Categories;
 }
