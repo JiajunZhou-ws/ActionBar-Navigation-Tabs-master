a560fb5ea25fd563e098e10606ade43b5ed291dd
Tonny Zhou <zhou_jiajun@foxmail.com>
2017-05-04 17:42:14 +0800

    temp release

--- ActionBar-Navigation-Tabs-master/MaterialDesign/app/src/main/java/com/djandroid/jdroid/Eab/ClientLibrary/Common/ClientConfiguration.java
+++ ActionBar-Navigation-Tabs-master/MaterialDesign/app/src/main/java/com/djandroid/jdroid/Eab/ClientLibrary/Common/ClientConfiguration.java
@@ -4,7 +4,7 @@ package com.djandroid.jdroid.Eab.ClientLibrary.Common;
  * Created by Jimmy on 2016/10/25.
  */
 public class ClientConfiguration {
-    public static String ServiceUrl = "http://207.46.149.137:5001/android";
+    public static String ServiceUrl = "http://207.46.149.137:5002/android";
     public static int Version = 10;
     //public static String ServiceUrl = "http://localhost:8080";
 }
